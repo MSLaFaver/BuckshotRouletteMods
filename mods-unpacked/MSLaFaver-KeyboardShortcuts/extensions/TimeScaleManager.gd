20433c37e7b74c7c9279363d03dff99192d11efb
ScientificGuy <93147703+ScientificGuy@users.noreply.github.com>
2024-03-03 22:14:07 -0800

    Create TimeScaleManager.gd

---
+++ mods-unpacked/MSLaFaver-KeyboardShortcuts/extensions/TimeScaleManager.gd
@@ -0,0 +1,16 @@
+extends "res://scripts/TimeScaleManager.gd"
+
+var disabled = false
+
+func _unhandled_input(event):
+	if (event.is_action_pressed("exit")):
+		disabled = !disabled
+
+func LerpTimeScale():
+	if (moving):
+		elapsed += get_process_delta_time()
+		var c = clampf(elapsed / timeScaleLerpDuration, 0.0 , 1.0)
+		c = ease(c, 0.4)
+		var val = lerpf(from, to, c)
+		if (!disabled):
+			Engine.time_scale = val
