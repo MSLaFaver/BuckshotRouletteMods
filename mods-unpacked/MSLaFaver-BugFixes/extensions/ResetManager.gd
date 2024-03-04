b893da3587387e253172486945e8531ccd4ef1c4
ScientificGuy <93147703+ScientificGuy@users.noreply.github.com>
2024-03-03 20:39:09 -0800

    Update ResetManager.gd
    Improper interaction with other time scale features

--- mods-unpacked/MSLaFaver-BugFixes/extensions/ResetManager.gd
+++ mods-unpacked/MSLaFaver-BugFixes/extensions/ResetManager.gd
@@ -1,6 +1,6 @@
 extends "res://scripts/ResetManager.gd"
 
 func _unhandled_input(event):
+	if (event.is_action_pressed("reset")):
+		Engine.time_scale = 1
 	super._unhandled_input(event)
-	Engine.time_scale = 1
-	
