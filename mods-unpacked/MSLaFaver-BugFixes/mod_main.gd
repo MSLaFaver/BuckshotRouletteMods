613df4ae0c6cb80198b89e47660b28e7424a24b1
ScientificGuy <93147703+ScientificGuy@users.noreply.github.com>
2024-03-09 16:46:33 -0800

    Add files via upload
    Changed dispenser texture

--- mods-unpacked/MSLaFaver-BugFixes/mod_main.gd
+++ mods-unpacked/MSLaFaver-BugFixes/mod_main.gd
@@ -32,9 +32,8 @@ func _process(delta):
 	if get_tree().get_root().get_child(2).name == "main":
 		var dispenser = get_tree().get_root().get_child(2).get_node("restroom_CLUB/Cube_112")
 		var material = dispenser.mesh.surface_get_material(0) as StandardMaterial3D
-		material.albedo_texture = Texture2D.new()
-		material.albedo_color = Color(0.192,0.220,0.192,1)
+		var image = Image.load_from_file("res://mods-unpacked/MSLaFaver-BugFixes/dispenser_clean.png")
+		var texture = ImageTexture.create_from_image(image)
+		material.albedo_texture = texture
 		dispenser.mesh.surface_set_material(0,material)
-	
-
 	
