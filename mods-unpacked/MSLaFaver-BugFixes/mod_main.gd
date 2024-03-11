5a1992a78982fe5f23bf382a85e08523d037ca20
ScientificGuy <93147703+ScientificGuy@users.noreply.github.com>
2024-03-10 19:01:01 -0700

    Add files via upload

--- mods-unpacked/MSLaFaver-BugFixes/mod_main.gd
+++ mods-unpacked/MSLaFaver-BugFixes/mod_main.gd
@@ -20,6 +20,7 @@ func install_script_extensions() -> void:
 	extensions_dir_path = mod_dir_path+"extensions/"
 	const extensions = [
 		'ResetManager',
+		'FilterController',	# Has nothing to do with the dispenser but used to run the update code only once per load
 	]
 	for extension in extensions:
 		ModLoaderMod.install_script_extension(extensions_dir_path+extension+".gd")
@@ -29,11 +30,5 @@ func _ready() -> void:
 	pass
 
 func _process(delta):
-	if get_tree().get_root().get_child(2).name == "main":
-		var dispenser = get_tree().get_root().get_child(2).get_node("restroom_CLUB/Cube_112")
-		var material = dispenser.mesh.surface_get_material(0) as StandardMaterial3D
-		var image = Image.load_from_file("res://mods-unpacked/MSLaFaver-BugFixes/dispenser_clean.png")
-		var texture = ImageTexture.create_from_image(image)
-		material.albedo_texture = texture
-		dispenser.mesh.surface_set_material(0,material)
+	pass
 	
