1f42eafc306f27d7615ca034ac80ab904fb1b53d
ScientificGuy <93147703+ScientificGuy@users.noreply.github.com>
2024-03-25 02:07:52 -0400

    FullCustomizer 1.0.0

--- mods-unpacked/MSLaFaver-FullCustomizer/mod_main.gd
+++ mods-unpacked/MSLaFaver-FullCustomizer/mod_main.gd
@@ -1,17 +1,11 @@
 extends Node
 
-
 const AUTHORNAME_MODNAME_DIR := "MSLaFaver-FullCustomizer"
 const AUTHORNAME_MODNAME_LOG_NAME := "MSLaFaver-FullCustomizer:Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
-var translations_dir_path := ""
 
-var ran_main = false
-
-# Before v6.1.0
-# func _init(modLoader = ModLoader) -> void:
 func _init() -> void:
 	mod_dir_path = ModLoaderMod.get_unpacked_dir()+(AUTHORNAME_MODNAME_DIR)+"/"
 	# Add extensions
@@ -21,12 +15,11 @@ func install_script_extensions() -> void:
 	extensions_dir_path = mod_dir_path+"extensions/"
 	const extensions = [
 		'RoundManager',
-		'ItemManager',
+		'ItemManager'
 	]
 	for extension in extensions:
 		ModLoaderMod.install_script_extension(extensions_dir_path+extension+".gd")
 
-	
 func _ready() -> void:
 	pass
 
